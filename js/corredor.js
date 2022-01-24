c5c482e3becd48c958f084499f09309da6e06839
MatteoCiro <mateo.agudeloc@upb.edu.co>
2022-01-24 17:29:16 -0500

    creamos una funcion contenedor que contenga un evento click y los botones atras y adelante

--- js/corredor.js
+++ js/corredor.js
@@ -1,2 +1,12 @@
-var imagenes = ['images/asteroide.jpg', 'images/luna.jpg', 'images/robot.jpg'], 
+var imagenes = ['img/asteroide.jpg', 'img/luna.jpg', 'img/robot.jpg'], 
     cont = 0;
+
+function corredor(contenedor){
+    contenedor.addEventListener('click', e) => {
+        let atras = contenedor.querySelector('.atras'),
+            adelante = contenedor.querySelector('.adelante'),
+            img = contenedor.querySelector('images'),
+            tgt = e.target;
+
+    }
+}
