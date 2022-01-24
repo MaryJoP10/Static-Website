27bb3feab83709d41281f06aac938a189cdcd535
MatteoCiro <mateo.agudeloc@upb.edu.co>
2022-01-24 17:31:56 -0500

    correccion de parentesis

--- js/corredor.js
+++ js/corredor.js
@@ -2,11 +2,11 @@ var imagenes = ['img/asteroide.jpg', 'img/luna.jpg', 'img/robot.jpg'],
     cont = 0;
 
 function corredor(contenedor){
-    contenedor.addEventListener('click', e) => {
+    contenedor.addEventListener('click', e => {
         let atras = contenedor.querySelector('.atras'),
             adelante = contenedor.querySelector('.adelante'),
             img = contenedor.querySelector('images'),
             tgt = e.target;
 
-    }
+    });
 }
